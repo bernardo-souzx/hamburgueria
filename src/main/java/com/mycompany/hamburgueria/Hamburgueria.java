eafeaaa73e44307f755aea7bc80e488b0f107e85
junio <junio@Fbio>
2026-04-24 19:50:44 -0300

    Alteração no main. (Teste)

--- src/main/java/com/mycompany/hamburgueria/Hamburgueria.java
+++ src/main/java/com/mycompany/hamburgueria/Hamburgueria.java
@@ -3,6 +3,6 @@ package com.mycompany.hamburgueria;
 public class Hamburgueria {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Olá Mundo");
     }
 }
